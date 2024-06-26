ff2bc3afd400f4d3e98d262b0360bd5bb7c6a69c
Doctor1234567890 <f984377833@gmail.com>
2024-06-26 14:56:21 +0200

    publisher finished

--- src/movingbox_gps/movingbox_gps/gps_sender.py
+++ src/movingbox_gps/movingbox_gps/gps_sender.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+
 from gps3 import gps3
 
 class GPS_Sender(Node):
 
     def __init__(self):
         super().__init__("gps_sender")
-        self.get_logger().info("hi.")
-        gps_socket = gps3.GPSDSocket()
-        data_stream = gps3.DataStream()
-        gps_socket.connect()
-        gps_socket.watch()
-        
-        for new_data in gps_socket:
-            if new_data:
-                data_stream.unpack(new_data)
-                self.get_logger().info('Altitude = ' + str(data_stream.TPV['alt']))
-                self.get_logger().info('Latitude = ' + str(data_stream.TPV['lat']))
-                self.get_logger().info('Longitude = ' + str(data_stream.TPV['lon']))
-                
 
+        self.gps_socket = gps3.GPSDSocket()
+        self.data_stream = gps3.DataStream()
+        self.gps_socket.connect()
+        self.gps_socket.watch()
+
+        self.create_timer(1, self.timer_callback)
+        self.publisher = self.create_publisher(Float64MultiArray, "/gps_sender", 10)
+                
+    def timer_callback(self):
+        new_data = self.gps_socket.next()
+        if not new_data: return
+
+        self.data_stream.unpack(new_data)
+        logger = self.get_logger()
+
+        msg = Float64MultiArray()
+        lat = self.data_stream.TPV['lat']
+        lon = self.data_stream.TPV['lon']
+        if lat == "n/a" or lon == "n/a": return logger.info("No data")
+
+        msg.data = [float(lat), float(lon)]
+        self.publisher.publish(msg)
 
         
 def main(args=None):
     rclpy.init(args=args)
     sender = GPS_Sender()
+    rclpy.spin(sender)
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- src/movingbox_gps/setup.py
+++ src/movingbox_gps/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "gps_sender = movingbox_gps.gps_sender:main"
         ],
     },
 )
